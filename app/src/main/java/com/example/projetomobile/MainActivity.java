36c7c898a11f274da1eab3d8031fd612b18efa9f
BeatrizVitoria <ms.beatrizvitoria@gmail.com>
2019-11-19 13:56:08 -0300

    envio

--- app/src/main/java/com/example/projetomobile/MainActivity.java
+++ app/src/main/java/com/example/projetomobile/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,14 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle s) {
         super.onCreate(s);
         setContentView(R.layout.activity_main);
-        Button btChoose = findViewById(R.id.bt_choose_destino);
-        btChoose.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
 
-                Intent intent = new Intent(MainActivity.this, Destino.class);
-                startActivity(intent);
-            }
-        });
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { // vai verificar qual foi o item selecionado pelo o usuário.
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
 }

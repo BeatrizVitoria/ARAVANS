3c928266d313bd8b5f152a64f4f67891cd75dadd
lucasmacario <lucasmacariohp@gmail.com>
2019-11-21 22:04:17 -0300

    icon - Lucas Macário

--- app/src/main/java/com/example/aravans/EscolhaDestino.java
+++ app/src/main/java/com/example/aravans/EscolhaDestino.java
@@ -5,8 +5,9 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EscolhaDestino extends AppCompatActivity {
+
     @Override
-    protected void OnCreate(Bundle b){
+    protected void onCreate(Bundle b){
         super.onCreate(b);
         setContentView(R.layout.escolha_destino);
     }
